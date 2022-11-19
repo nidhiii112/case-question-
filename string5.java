36da9f6befbb6e307e649901b3687447aa7e18cf
Nidhi <nidhii950898@gmail.com>
2022-11-19 20:24:12 +0530

    second commit

--- string5.java
+++
@@ -1,7 +0,0 @@
-//program to fill a letter templet
-
-import java.util.*;
-import java.util.Scanner;
-public class string3 {
-public static void main(String[] args) {
-    String string = "uppercase";
