592f2ca0f7feda0abfe9a83526a8952b6498c9d3
Nidhi singh <117963273+nidhiii112@users.noreply.github.com>
2023-04-05 08:08:10 +0530

    Update string6.java

--- string6.java
+++ string6.java
@@ -1,5 +1,5 @@
 /*program to fill in letter tamplet which looks like
-letter = "dear<|name|.,Thanks a lot"
+letter = "dear<|name|., Thanks a lot"
 Replace <|name|> with string ( some name)*/
 import java.util.*;
 public class string6{
